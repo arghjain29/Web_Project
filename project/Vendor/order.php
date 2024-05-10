ff9de7c52bddd2613edfbe7b8b39def241b37f0d
arghjain29 <148798022+arghjain29@users.noreply.github.com>
2024-05-10 15:35:57 +0530

    Final

--- project/Vendor/order.php
+++ project/Vendor/order.php
@@ -1,4 +1,3 @@
-<html>
 <style>
       .pdt-card {
         background-color: lightpink;
@@ -32,40 +31,39 @@
         font-size: 18px;
         font-family:'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
       }
-      .cancel {
-        margin-top: 40px;
-        text-align: center;
+      .cust {
+        margin-top: 10px;
+        margin-bottom: 10px;
+        padding: 2%;
+        background-color: lightcoral;
+        font-size: 18px;
+        font-family:'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+        font-weight: 500;
       }
-    </style>
-</html>
+</style>
+
+
 
 <?php
-
 include "authguard.php";
 include_once "../shared/connection.php";
 include "menu.html";
 
-echo "<h1><center>Recent Orders</center></h1>";
+$sql_result = mysqli_query($conn, "select * from product join order_list on product.pid=order_list.pid where owner=$_SESSION[userID]");
 
-$total=0;
 
-$sql_result = mysqli_query($conn, "SELECT * FROM order_list JOIN product ON order_list.pid=product.pid WHERE userid=$_SESSION[userID]");
+    while ($dbrow= mysqli_fetch_assoc($sql_result))
+    {
+      
+      echo "
+      <div class='pdt-card'>
+           <div class='name'>$dbrow[name]</div>
+           <div class='price'>$dbrow[price]</div>
+           <img class='pdtimg' src='$dbrow[impath]'>
+           <div class='detail'>$dbrow[detail]</div>
+           <div class='cust'>Customer ID = $dbrow[userid]</div>
+      </div>";
+    }
 
-while ($dbrow = mysqli_fetch_assoc($sql_result)) {
-    $total+=$dbrow['price'];
+?>
 
-    echo "
-    <div class='pdt-card'>
-         <div class='name'>$dbrow[name]</div>
-         <div class='price'>$dbrow[price]</div>
-         <img class='pdtimg' src='$dbrow[impath]'>
-         <div class='detail'>$dbrow[detail]</div>
-    </div>";
-
-}          
-echo "<div class='cancel'>
-    <h2>Order Total Amount: Rs $total</h2>
-    <a href='Cancel.php'><button class='btn btn-danger mt-2'>Cancel Order</button></a>
-  </div>";
-
-?>  
